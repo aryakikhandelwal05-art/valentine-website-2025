23dc6152269cfa504ad2210bf3616890e984e5a3
aryakikhandelwal05-art <aryaki.khandelwal05@gmail.com>
2026-02-03 20:02:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Yuggu",
+    valentineName: "Yushi",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
